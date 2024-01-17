a9c36550e059b40ad2b1cbc9e57ada48e1540173
fazleyrabby <fazley111@gmail.com>
2024-01-17 23:01:43 +0600

    font size update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,6 +17,16 @@ export default {
 		  'serif' : ['Lora','serif'],
 		  'logo' : ['Comforter Brush','system-ui'],
 		},
+		fontSize: {
+			xs: '0.7rem',
+			sm: '0.8rem',
+			base: '0.95rem',
+			xl: '1.15rem',
+			'2xl': '1.563rem',
+			'3xl': '1.953rem',
+			'4xl': '2.441rem',
+			'5xl': '3.052rem',
+		  },
 		container: {
 		  padding: {
 			DEFAULT: '1rem',

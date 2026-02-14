2cc976f87a50d7c0c9527189bfa2a9748af2c84a
Md. Fazley Rabbi <fazley111@gmail.com>
2026-02-14 19:36:56 +0600

    fix: theme issue fix in posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,13 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+    },
+  },
 });

1f453987a5a70338db10f95a420b9b8de9ef5580
Md. Fazley Rabbi <fazley111@gmail.com>
2026-03-14 00:01:52 +0600

    build issue fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 
 export default defineConfig({
+  output: "server",
   adapter: netlify(),
   integrations: [tailwind()],
 
